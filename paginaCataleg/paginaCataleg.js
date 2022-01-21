0cf0fe0e6e87623cab471c40e32a40547d8c967e
marcmiquelmarti <marc.miquel@institutvidreres.cat>
2022-01-21 08:25:07 +0100

    Dia 2101

---
+++ paginaCataleg/paginaCataleg.js
@@ -0,0 +1,3 @@
+function producteAfegit(URL) {
+    window.open(URL, 'Avís de cistella!', 'toolbar=0, scrollbars=0, location=0, statusbar=0, menubar=0, resizable=1, width=300, height=200, left = 390, top=50');
+}
